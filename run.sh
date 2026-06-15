955bcadbcf859d900e974afbd4d8eab1cf08ddc2
ByteBandit-100 <ghsmohit2004@gmail.com>
2026-06-15 14:48:32 +0000

    updated ssh_analyzer to ssh_auth_analyzer and output

--- run.sh
+++ run.sh
@@ -5,21 +5,23 @@ echo "================================================"
 echo -en "\e[0m"
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <logfile> or </path/logfile>"
-    echo -e "\e[1;33m================================================\e[0m"
-    exit 1
+	echo "Usage: $0 <logfile> or </path/logfile>"
+	echo -e "\e[1;33m================================================\e[0m"
+	exit 1
 fi
 
 if [[ ! -f "$1" ]]; then
-    echo "FILE NOT FOUND :<"
-    echo -e "\e[1;33m================================================\e[0m"
-    exit 1
-    
+	echo "FILE NOT FOUND :<"
+	echo -e "\e[1;33m================================================\e[0m"
+	exit 1
+
 fi
 
-if [[ "$1" == *"apache"* ]]; then		
-			./scripts/apache_log_analyzer.sh "$1" | tee ./reports/apache_report.txt				
+if [[ "$1" == *"access.log"* || "$1" == *"apache"* ]]; then
+	./scripts/apache_log_analyzer.sh "$1" | tee ./reports/apache_report.txt
+elif [[ "$1" == *"auth.log"* || "$1" == *"ssh"* || "$1" == *"secure"* || $1 == *"auth"* ]]; then
+	./scripts/ssh_auth_analyzer.sh "$1" | tee ./reports/ssh_auth_report.txt
 else
-    echo "Unsupported log type"
+	echo "Unsupported log type"
 fi
 echo -e "\e[1;33m================================================\e[0m"
---
+++ scripts/ssh_auth_analyzer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+total_ssh_events=$(grep -c "sshd" "$1")
+failed_pswd_attempts=$(grep -c "Failed password" "$1")
+successfull_logins=$(grep -Ec "Accepted password|Accepted publickey" "$1")
+invalid_user_attempts=$(grep -c "Invalid user" "$1")
+successfull_login_rt=$(grep -Ec "Accepted password for root|Accepted publickey for root" "$1")
+failed_login_rt=$(grep -c "Failed password for root" "$1")
+
+echo -e "\e[1m******** SSH FINDINGS ********\e[0m"
+echo "Total SSH events : $total_ssh_events"
+echo "Failed login attempts : $failed_pswd_attempts"
+echo "Successfull logins : $successfull_logins"
+echo "Invalid user attempts : $invalid_user_attempts"
+
+echo "------- TOP TARGETS "
+echo "$(
+	{
+		grep "Failed password" "$1" |
+			awk '{
+for(i=1;i<=NF;i++){
+if($i=="for" && $(i+1)=="invalid" && $(i+2)=="user")
+print $(i+3)
+else if($i=="for" && $(i+1)!="invalid")
+print $(i+1)
+}}'
+		grep "Invalid user" "$1" |
+			awk '{
+for(i=1;i<=NF;i++)
+if($i=="Invalid" && $(i+1)=="user")
+print $(i+2)}'
+	} | sort | uniq -c | sort -nr | awk '{print $2 " : " $1}'
+)"
+
+echo "------- TOP ATTACKER IPS"
+echo -e "IP\t\t| ATTEMPTS"
+echo "$(grep -Ei "invalid user|failed password" "$1" | awk '{for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' | sort | uniq -c | sort -nr | awk '{print $2 "\t:  " $1}')"
+echo "------- Root login attempts"
+echo "Successfull attempts : $successfull_login_rt"
+echo "Failed attempts : $failed_login_rt"
+echo "------- BRUTE FORCE ALERTS "
+echo -e "$(
+	grep "Failed password" "$1" |
+		awk '{for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' |
+		sort | uniq -c | sort -nr |
+		awk '{ 
+    if ($1 >= 10) 
+        print "[!] ALERT: " $2 " (" $1 " failed attempts)"; 
+    else 
+        print "[?] Suspicious: " $2 " (" $1 " failed attempts)" 
+}'
+)"
